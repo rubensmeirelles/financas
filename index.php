4811d20b53613f1ff682d6c17c472eada30dc7ad
Rubens de Deus Meirelles <rubens.meirelles@postalsaude.com.br>
2024-03-13 09:05:58 -0300

    Incluindo script tailwindcss

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Controle Financeiro Pessoal</title>
 </head>
 <body>
-    
+    <h1 class="text-3xl font-bold underline">
+        Dashboard
+    </h1>
 </body>
 </html>
